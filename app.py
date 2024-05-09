20dba1ce3e7b5a9785335b6ad8bfbf8a2b7a238b
ptwitchett <p.twitchett@aol.co.uk>
2024-05-09 22:29:22 +0100

    MVP

--- app.py
+++ app.py
@@ -1,50 +1,53 @@
-# app.py
-from flask import Flask, render_template, request, redirect
+import csv
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-# List to store brews
-brews = []
+def load_ingredients():
+    with open('ingredients.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return list(reader)
+
+ingredients = load_ingredients()
 
 @app.route('/')
 def index():
     return render_template('index.html', brews=brews)
 
-@app.route('/new-brew')
-def new_brew():
-    return render_template('new_brew.html')
-
-@app.route('/save-brew', methods=['POST'])
-def save_brew():
-    if request.method == 'POST':
-        brew = {
-            "name": request.form['brew-name'],
-            "start_date": request.form['start-date'],
-            "total_volume": request.form['total-volume'],
-            "start_sg": request.form['start-sg'],
-            "ingredients": request.form['ingredients']
-        }
-        brews.append(brew)
-        return redirect('/')
-    return render_template('index.html', brews=brews)
-
 @app.route('/brew/<int:brew_id>')
 def brew_details(brew_id):
-    brew = brews[brew_id]
-    return render_template('brew_details.html', brew=brew)
+    brew = get_brew_by_id(brew_id)
+    latest_update = get_latest_update(brew_id)
+    if latest_update:
+        latest_contents = latest_update.get('contents', brew['ingredients'])
+    else:
+        latest_contents = brew.get('ingredients', '')
+    return render_template('brew_details.html', brew=brew, latest_contents=latest_contents, brew_id=brew_id)
+
+@app.route('/delete-brew/<int:brew_id>', methods=['POST'])
+def delete_brew(brew_id):
+    if request.method == 'POST':
+        # Delete brew code here
+        return redirect('/')
+
+@app.route('/new-brew')
+def new_brew():
+    # New brew code here
+    return redirect('/')
 
 @app.route('/abv-calculator')
 def abv_calculator():
-    return render_template('abv_calculator.html')
+    # ABV calculator code here
+    return render_template('abv_calculator.html', abv=round(abv, 2))
 
-@app.route('/calculate-abv', methods=['POST'])
-def calculate_abv():
+@app.route('/ingredients', methods=['GET', 'POST'])
+def show_ingredients():
     if request.method == 'POST':
-        start_sg = float(request.form['start-sg'])
-        end_sg = float(request.form['end-sg'])
-        abv = (start_sg - end_sg) * 131.25
-        return render_template('abv_calculator.html', abv=abv)
-    return redirect('/abv-calculator')
+        selected_category = request.form.get('category')
+        filtered_ingredients = [ingredient for ingredient in ingredients if ingredient['category'] == selected_category]
+        return render_template('ingredients.html', ingredients_list=filtered_ingredients, selected_category=selected_category)
+    else:
+        return render_template('ingredients.html', ingredients_list=ingredients, selected_category=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
