c9c25e8e97c4e22ae16d8550f968f2a70823bb45
ptwitchett <p.twitchett@aol.co.uk>
2024-05-15 15:54:23 +0100

    Notesadded

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import csv
+import random
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 # list to store brews
 brews = []
+
+user_notes = []
 
 #loads csv file for ingredients
 def load_ingredients():
@@ -96,14 +99,35 @@ def calculate_abv():
     return redirect('/abv-calculator')
 
 #renders all ingredients in /ingredients unless a category is chosen, then only that category of ingredient is shown
+
 @app.route('/ingredients', methods=['GET', 'POST'])
 def show_ingredients():
+    random_ingredient = random.choice(ingredients)
     if request.method == 'POST':
         selected_category = request.form.get('category')
         filtered_ingredients = [ingredient for ingredient in ingredients if ingredient['category'] == selected_category]
-        return render_template('ingredients.html', ingredients_list=filtered_ingredients, selected_category=selected_category)
+        return render_template('ingredients.html', ingredients_list=filtered_ingredients, selected_category=selected_category, random_ingredient=random_ingredient)
     else:
-        return render_template('ingredients.html', ingredients_list=ingredients, selected_category=None)
+        return render_template('ingredients.html', ingredients_list=ingredients, selected_category=None, random_ingredient=random_ingredient)
+    
+@app.route('/random-ingredient', methods=['GET'])
+def random_ingredient():
+    random_ingredient = random.choice(ingredients)
+    return render_template('random_ingredient.html', random_ingredient=random_ingredient)
+
+# Renders the note page where users can type notes
+@app.route('/notes')
+def show_notes():
+    return render_template('notes.html', notes=user_notes)
+
+# Saves the note entered by the user and redirects back to the notes page
+@app.route('/save-note', methods=['POST'])
+def save_note():
+    if request.method == 'POST':
+        note = request.form['note']
+        user_notes.append(note)
+    return redirect('/notes')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
